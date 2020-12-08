30f49a540d037376781b8d8b9f644e3b29de19fa
sankhusky <pimple.s@husky.neu.edu>
2020-12-08 01:48:48 -0500

    first comit, created basic structure, fixed delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import DeleteButton from "./DeleteButton";
+class App extends Component {
+  state = {
+    fileDropDownList: [],
+    deleteLabel: null,
+    newUpload : false
+  };
+//   TODO: Add progress dialog for upload
+  deleteButtonLabel = "Select file to delete ->";
+  // fileList = [];
+  // fileDropDownList = [];
+  baseUrl = "http://192.168.0.42:8000";
+  // Play API call
+  playMusic = () => {
+    let url = `${this.baseUrl}/music/play`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      // .then((result) => result.json())
+      .then((result) => {
+        // this.setState({
+        //   result: result,
+        // });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  stopMusic = () => {
+    let url = `${this.baseUrl}/music/stop`;
+    // console.log(url);
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      // .then((result) => result.json())
+      .then((result) => {
+        // console.log(result);
+        // this.setState({
+        //   result: result,
+        // });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getAllFiles = () => {
+    let url = `${this.baseUrl}/music/music_files`;
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  };
+
+  uploadFile = () => {
+    const formData = new FormData();
+    const fileField = document.querySelector('input[type="file"]');
+
+    // formData.append("username", "abc123");
+    formData.append("file", fileField.files[0]);
+
+    fetch(`${this.baseUrl}/music/file`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+        this.setState({
+          newUpload : true
+        });
+        // -----------------re-populating the files -----------------
+      //   this.getAllFiles()
+      //     .then((result) => result.json())
+      //     .then((result) => {
+      //       this.setState({
+      //         fileDropDownList: result,
+      //       });
+      //     })
+      //     .catch((err) => {
+      //       console.log(err);
+      //     });
+      // })
+      // .catch((error) => {
+      //   console.error("Error:", error);
+      });
+  };
+
+  setVolume = () => {
+    var slider = document.getElementById("myRange");
+    let volumeLevel = slider.value / 100;
+    console.log(volumeLevel);
+    // const formData = new FormData();
+    // formData.append("volume", volumeLevel);
+    let url = `${this.baseUrl}/music/volume?volume=${volumeLevel}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      // body: formData,
+    })
+      // .then((result) => result.json())
+      .then((result) => {
+        // console.log(result);
+        // this.setState({
+        //   result: result,
+        // });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  pauseToggle = false; //if true, the audio is paused
+  pauseMusic = () => {
+    let url;
+    if (this.pauseToggle==true) {
+      url = `${this.baseUrl}/music/unpause`;
+    } else {
+      url = `${this.baseUrl}/music/pause`;
+    }
+    // console.log(url);
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((result) => {
+        this.pauseToggle = !this.pauseToggle;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>MMB Tester</h1>
+        <ol>
+          <li>
+            Music:
+            <button type="button" onClick={this.playMusic}>
+              Play
+            </button>
+            <button type="button" onClick={this.stopMusic}>
+              Stop
+            </button>
+            <button type="button">Pause</button>
+            <br />
+            Volume:
+            <input
+              type="range"
+              min="1"
+              max="100"
+              // value="100"
+              className="slider"
+              id="myRange"
+              onChange={this.setVolume}
+            />
+          </li>
+          <li>
+            Update Music File:
+            <input
+              type="file"
+              id="file_input"
+              onChange={this.uploadFile}
+            ></input>
+          </li>
+          <li>
+            Delete Music File:
+            <DeleteButton newUpload={this.state.newUpload}/>
+          </li>
+          <li>
+            LED Strip:
+            <input className="tgl tgl-light" id="cb1" type="checkbox" />
+            <label className="tgl-btn"></label>
+          </li>
+          <li>
+            Movement (dance) :
+            <input className="tgl tgl-light" id="cb1" type="checkbox" />
+          </li>
+          <li>
+            Mystery Box (ring):
+            <input className="tgl tgl-light" id="cb1" type="checkbox" />
+          </li>
+          <li>
+            Trigger Spell:
+            <button type="button">Locomotor</button>
+            <button type="button">Arresto Momentum</button>
+            <button type="button">Revelio</button>
+          </li>
+          <li>Camera View</li>
+          {/* Add component here */}
+          <li>Wand movement view:</li>
+          {/* Add component here */}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/DeleteButton.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+// import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+class DeleteButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleteLabel: "Select file to delete ->",
+      fileDropDownList: [],
+      deleteFileName: null,
+      newUpload: props.newUpload,
+    };
+  }
+// TODO: Update the dropdown list after file delete success
+// TODO: Clear all warnings, use recommended stuff
+  baseUrl = "http://192.168.0.42:8000";
+  getAllFiles = () => {
+    let url = `${this.baseUrl}/music/music_files`;
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  };
+
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    //   console.log("new upload caught in child component");
+    //   console.log(this.props.newUpload);
+    //   console.log(prevProps.newUpload);
+    if (this.props.newUpload == true && prevProps.newUpload==false ) {
+      populateFiles(this);
+    }
+  }
+  componentDidMount() {
+    populateFiles(this);
+  }
+
+  changeButtonLabel = (fileName) => (e) => {
+    console.log(" trying to delete : " + fileName);
+    this.setState({
+      deleteLabel: "Delete '" + fileName + "'",
+      deleteFileName: fileName,
+    });
+  };
+
+  deleteFile = () => {
+    let url = `${this.baseUrl}/music/file?filename=${this.state.deleteFileName}`;
+    // console.log(url);
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      // .then((result) => result.json())
+      .then((result) => {
+        this.setState({
+          deleteLabel: "Select file to delete ->",
+        });
+        alert("File deleted successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong, please select an audio file");
+      });
+  };
+  render() {
+    // console.log("in render");
+    const { fileDropDownList } = this.state;
+
+    return (
+      <Dropdown as={ButtonGroup}>
+        <Button variant="danger" onClick={this.deleteFile}>
+          {this.state.deleteLabel}{" "}
+        </Button>
+
+        <Dropdown.Toggle split variant="danger" id="dropdown-split-basic" />
+
+        <Dropdown.Menu>{this.state.fileDropDownList}</Dropdown.Menu>
+      </Dropdown>
+    );
+  }
+}
+function populateFiles(context) {
+  context
+    .getAllFiles()
+    .then((result) => result.json())
+    .then((result) => {
+      const dropDownButtons = result.map((fileName, index) => {
+        return (
+          <Dropdown.Item
+            key={index}
+            onClick={context.changeButtonLabel(fileName)}
+          >
+            {fileName}
+          </Dropdown.Item>
+        );
+      });
+      context.setState({
+        fileDropDownList: dropDownButtons,
+        newUpload: false,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export default DeleteButton;
